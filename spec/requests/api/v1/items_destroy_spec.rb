281354e1e0cdd6212a0aaf774a4dd1e93d834463
Brendan Dillon <br.lyle.dillon@gmail.com>
2016-11-02 09:32:02 -0600

    Create test for items destroy API endpoint

---
+++ spec/requests/api/v1/items_destroy_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'Items destroy API endpoint' do
+  it 'deletes one item' do
+    item1 = Item.create(name: "mug", description: "striped", image_url: "http://google.com/mug")
+    item2 = Item.create(name: "table", description: "wooden", image_url: "http://flickr.com/table")
+
+    delete "/api/v1/items/#{item1.id}"
+
+    result = JSON.parse(response.body)
+
+    expect(response.status).to eq(204)
+    expect(Item.find(item1.id)).to eq(nil)
+  end
+end
