2dbdec7a4a46939b40e6f403945d34fae4942989
Brendan Dillon <br.lyle.dillon@gmail.com>
2016-11-02 10:16:57 -0600

    Add accurate expectations to store search spec

--- spec/features/user_sees_nearby_stores_spec.rb
+++ spec/features/user_sees_nearby_stores_spec.rb
@@ -2,23 +2,19 @@ require 'rails_helper'
 
 RSpec.describe 'A user searches for their zip code' do
   it "sees the stores within 25 miles" do
-#As a user
-#When I visit "/"
     visit '/'         
-#And I fill in a search box with "80202" and click "search"
-    fill_in('zip'), with: '80202'
+    fill_in 'zip', with: '80202'
     click_on 'Search'
-#Then my current path should be "/search" (ignoring params)
+    
     expect(current_path).to eq('/search')
-#And I should see stores within 25 miles of 80202
-    expect 
-#And I should see a message that says "17 Total Stores"
     expect(page).to have_content('17 Total Stores')
-#And I should see exactly 15 results
     expect(page).to have_css('.store', count: 15)
-#And I should see the long name, city, distance, phone number and store type for each of the 15 results
     within ('.store:first') do
-      expect(page).to have_content('blah')
+      expect(page).to have_content("Best Buy Mobile - Cherry Creek Shopping Center")
+      expect(page).to have_content("Denver")
+      expect(page).to have_content("3.25")
+      expect(page).to have_content("303-270-9189")
+      expect(page).to have_content("Mobile")
     end
   end
 end
