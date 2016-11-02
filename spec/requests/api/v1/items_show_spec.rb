e83ce516752dde451166fa89312e3e03cee57592
Brendan Dillon <br.lyle.dillon@gmail.com>
2016-11-02 09:27:25 -0600

    Create items show API endpoint test

---
+++ spec/requests/api/v1/items_show_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'Items show API endpoint' do
+  it 'returns one item' do
+    item1 = Item.create(name: "mug", description: "striped", image_url: "http://google.com/mug")
+    item2 = Item.create(name: "table", description: "wooden", image_url: "http://flickr.com/table")
+
+    get "/api/v1/item/#{item1.id}"
+
+    result = JSON.parse(response.body)
+
+    expect(response.status).to eq(200)
+    expect(result).to eq({"id" => item1.id, "name" => "mug", "description" => "striped", "image_url" => "http://google.com/mug"})
+  end
+end
